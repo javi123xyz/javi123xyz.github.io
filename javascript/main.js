e320b23231fb8a84e9fcb8c9896948a5d291a568
javi123xyz <Javiersolis@protonmail.com>
2025-04-11 23:29:01 -0700

    April 2025 Update
    Major overhaul in aesthetics, functionality, content

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-var wavesurfer = WaveSurfer.create({
-  container: '#waveform',
-  waveColor: 'darkorange',
-  progressColor: 'purple',
-  splitChannels: true,
-  height: 64
-});
-
-wavesurfer.load('https://wavesurfer-js.org/example/split-channels/stereo.mp3');
